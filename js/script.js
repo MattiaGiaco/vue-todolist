3e299a0bb824576da763927ec6b9d702f3ce4d66
mattiagiaco <martex781227@gmail.com>
2021-11-12 16:22:20 +0100

    milestone1 done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,28 @@ const app = new Vue({
 
   el: '#app',
   data:{
-    
+    todoList:[
+      {
+        text: 'Ripassare HTML/CSS',
+        check: false
+      },
+      {
+        text: 'Studiare Vue',
+        check: false
+      },
+      {
+        text: 'Fare la spesa',
+        check: true
+      },
+      {
+        text: 'Comprare pane',
+        check: false
+      }
+    ],
+    stringTodo: {
+      text: '',
+      check: false
+    }
   },
   methods:{
     
