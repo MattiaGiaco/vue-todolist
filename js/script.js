2557d48d47a9de7d98f435a2ab52281ddd454341
mattiagiaco <martex781227@gmail.com>
2021-11-12 16:58:23 +0100

    milestone 3 and bonus complete

--- js/script.js
+++ js/script.js
@@ -16,18 +16,33 @@ const app = new Vue({
         check: true
       },
       {
-        text: 'Comprare pane',
+        text: 'Comprare regali',
         check: false
       }
     ],
-    stringTodo: {
+    newItem: {
       text: '',
       check: false
-    }
+    },
+    error: false
   },
   methods:{
     removeItem(index){
       this.todoList.splice(index,1);
+    },
+    addItem(){
+      if (this.newItem.text.length < 3){
+        this.error = true;
+        setTimeout(()=>{
+          this.error = false;
+        },3000);
+      }else{
+        this.todoList.push(this.newItem)
+        this.newItem= {
+          text: '',
+          check: false
+        };
+      }
     }
     }
 });
